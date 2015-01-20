530053598a2795ac10f4b79be0c779f1fd9c8961
DeepwaterCreations <dylan.craine@deepwatercreations.com>
2015-01-20 13:55:24 -0700

    Lookit, we can files now!

---
+++ test/FileReadWrite.js
@@ -0,0 +1,36 @@
+﻿var filepath = "";
+
+//When a file is selected, get the filepath and set 'filepath' to it.
+$('#chooseFile').change(function (event) {
+    filepath = $('#chooseFile').prop("value");
+    console.log(filepath);
+});
+
+//When the read file button is clicked, read the file and put its contents in the div. 
+var fs = require('fs');
+$('#readFileButton').click(function () {
+    if (filepath === "") {
+        console.log("Error: No file selected");
+        return;
+    }
+
+    var filer = fs.createReadStream(filepath);
+    filer.setEncoding('utf8');
+    filer.on('data', function (chunk) {
+        console.log(chunk);
+        $('#fileTextBox').val(chunk);
+    })
+});
+
+
+//When the write file button is clicked, write the contents of the div's text box to
+//the file specified by 'filepath'.
+$('#writeFileButton').click(function () {
+    if (filepath === "") {
+        console.log("Error: No file selected");
+        return;
+    }
+
+    var filew = fs.createWriteStream(filepath);
+    filew.write($('#fileTextBox').val());
+})
