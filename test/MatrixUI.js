a6fa68f87366ae09f9530d2174ed67cc7d44d280
DeepwaterCreations <dylan.craine@deepwatercreations.com>
2015-01-22 13:37:50 -0700

    Starting to put the matrix UI together. Right now, MatrixUI.js is just comments about what I'm going to do.

---
+++ test/MatrixUI.js
@@ -0,0 +1,11 @@
+﻿//So we have rows associated with rights, and columns associated with impacted individuals, and the cells should hold the values.
+//We also need to reflect that this data has changed over time. 
+//Tooltips: Reasons for the scores, maybe also trend over time. 
+//Also, we want to filter impacted rights holders so we don't see all of them.
+
+//index.html can hold a blank table, like for the rows table.
+//We want to: Iterate over the rights list, and for each item, add a table data with the name and then a series of table datas with the values for the list of impacted rights-holders.
+//We also want to listen to the appropriate data values.
+//And, we'll want tool tips on the table data elements. (So they'll need title='' !)
+
+
