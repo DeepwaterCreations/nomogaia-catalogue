ef9bfc77799eb942f240ef29b22df30321d2e574
DeepwaterCreations <dylan.craine@deepwatercreations.com>
2015-02-19 15:43:15 -0700

    Changes the way rows are added to the matrix to make them easier to sort and filter.

--- test/MatrixUI.js
+++ test/MatrixUI.js
@@ -25,8 +25,6 @@ function Matrix() {
     //We also don't want to do this unless something has legitimately changed.
     //Maybe I want a monitor data structure lurking behind the UI that can keep track of such things?
     this.rebuild = function (monitor) {
-        var nodata = true;
-
         var matrixTableID = this.matrixTablePrefix;
 
         if (!monitorTables.backingData[monitor]) {
@@ -43,20 +41,27 @@ function Matrix() {
         //Then, rebuild it.
         $("#" + matrixTableID).append('<thead><tr><th class="columnHeader"></th></tr></thead>'); //Contains a blank <th> so there's space for a column of row names.
         $("#" + matrixTableID).append("<tbody></tbody>");
+  
+        var rowHTMLList = [];
         
         //Add the rows
-        //For each right, get the list of table rows that contain that right. Iterate over the rights-holders, and for each one that the row item impacts, get the score and increment a count of scores.
-        //Then put the averages in the table.
+        //For each right, get the list of monitor table rows that contain that right. Iterate over the rights-holders, and for each one that the row item impacts, get the score and increment a count of scores.
+        //Cell HTML strings go in an HTMLrow object mapped to rights-holder names. Then we can sort by rights and add rows in the rights order, or sort by rights-holders to add cells from each right in a particular column order.
         options.getColumnOptions("Impacted Rights").forEach(function (rightName) {
             rightName = rightName || undefinedRightNameFiller;
 
-            $("#" + matrixTableID).find("tbody").append('<tr id="' + getRowID(rightName) + '"></tr>');
-            $("#" + matrixTableID).find("tbody").find('tr').last().append('<th title="" class="rowHeader">' + rightName + '</th>');
+            var rowHTML = {
+                header: '<th title="" class="rowHeader">' + rightName + '</th>',
+                rowTag: '<tr id="' + getRowID(rightName) + '">',
+                rowCloseTag: '</tr>',
+                rowScore: undefined //A score of 0 is different from no score at all, yeah?
+            }
+
+            //$("#" + matrixTableID).find("tbody").append('<tr id="' + getRowID(rightName) + '"></tr>');
+            //$("#" + matrixTableID).find("tbody").find('tr').last().append('<th title="" class="rowHeader">' + rightName + '</th>');
 
             //Generate the scores and push them into the htmlString.
             var rows = data.getRows("Impacted Rights", rightName);
-            if (rows.length > 0)
-                nodata = false;
             options.getColumnOptions("Impacted Rights-Holders").forEach(function (rightsholderName) {
                 rightsholderName = rightsholderName || undefinedRightsHolderNameFiller;
 
@@ -73,40 +78,74 @@ function Matrix() {
                         tooltipContent += '<p>' + row["Input"] + '</p>';
                     }
                 });
+                var cell = {
+                    HTML: "",
+                    score: "-"
+                };
                 if (scoreCount > 0) {
+                    rowHTML.rowScore = (rowHTML.rowScore ? rowHTML.rowScore + scoreSum : scoreSum);
                     var avg = scoreSum / scoreCount;
-                    $("#" + matrixTableID).find("tbody").find('tr').last().append('<td title="" class="' + cellClass.rowClass + ', ' + cellClass.colClass + '">' + avg + '</td>');
+                    //$("#" + matrixTableID).find("tbody").find('tr').last().append('<td title="" class="' + cellClass.rowClass + ', ' + cellClass.colClass + '">' + avg + '</td>');
+                    cell.HTML = '<td title="" class="' + cellClass.rowClass + ', ' + cellClass.colClass + '">' + avg + '</td>';
                     //Also add a tooltip.
-                    var cell = $("#" + matrixTableID).find("tbody").find('tr').last().children().last();
-                    cell.tooltip({ content: tooltipContent });
-                    addScoreCategoryClass(cell, avg);
+                    //var cell = $("#" + matrixTableID).find("tbody").find('tr').last().children().last();
+                    //cell.tooltip({ content: tooltipContent });
+                    cell.tooltipContent = tooltipContent; //Leave this undefined to have no tooltip.
+                    //addScoreCategoryClass(cell.HTML, avg); //TODO: I'm not sure if this works. Can I change that function to return a bit of HTML I can stick into the string?
+                    cell.score = avg;
                 }
-                else
-                    $("#" + matrixTableID).find("tbody").find('tr').last().append('<td class="' + cellClass.rowClass + ', ' + cellClass.colClass + '">-</td>');
-                //When the user mouses over a cell, this makes the cell's column header become highlighted.
-                //Row headers already do this via pure CSS. (You can put a hover selector on the <tr> to find the appropriate row head, but the column heads aren't exclusively enclosed
-                //in an element with the cells below them, so we have to resort to javascript.) 
-                $("#" + matrixTableID).find("tbody").find('tr').last().children().last().hover(function (event) {
-                    //On mouse hover, give the column header a class.
-                    $('#' + getColumnHeadID(rightsholderName)).addClass("hoveredColumn");
-                },
-                function (event) {
-                    //On mouse hover end, remove the class.
-                    $('#' + getColumnHeadID(rightsholderName)).removeClass("hoveredColumn");
-                });
+                else {
+                    //$("#" + matrixTableID).find("tbody").find('tr').last().append('<td class="' + cellClass.rowClass + ', ' + cellClass.colClass + '">-</td>');
+                    cell.HTML = '<td class="' + cellClass.rowClass + ', ' + cellClass.colClass + '">-</td>';
+                }
+                rowHTML[rightsholderName] = cell;
+
             });
+            //This filters out the rows with no scoring cells.
+            if(rowHTML.rowScore !== undefined)
+                rowHTMLList.push(rowHTML); 
         });
-        if (nodata) {
+
+        if (!rowHTMLList.length) {
             $("#" + matrixTableID).empty();
             $("#" + this.divID).append('<div class="nodata"><span>NO DATA</span></div>');
         }
         else {
 
             //Add the column headings
+            //TODO: Here is where we sort columns. (Also make sure it's the same sorted list in the forEach inside the row adding.)
             options.getColumnOptions("Impacted Rights-Holders").forEach(function (rightsholderName) {
                 rightsholderName = rightsholderName || undefinedRightsHolderNameFiller; //This is a bit goofy, but in practice, I think we shouldn't have this ever. If we see it, it's an error. 
 
                 $("#" + matrixTableID).find("thead").find("tr").append('<th title="" id="' + getColumnHeadID(rightsholderName) + '" class="columnHeader">' + rightsholderName + '</th>');
+            });
+
+            //Add the cells
+            //TODO: Here is where we sort rows.
+            rowHTMLList.forEach(function (rowHTML) {
+                $("#" + matrixTableID).find("tbody").append(rowHTML.rowTag + rowHTML.header + rowHTML.rowCloseTag);
+                options.getColumnOptions("Impacted Rights-Holders").forEach(function (rightsholderName) {
+                    //Add the cell
+                    $("#" + matrixTableID).find("tbody").find('tr').last().append(rowHTML[rightsholderName].HTML);
+                    var cell = $("#" + matrixTableID).find("tbody").find('tr').last().children().last(); //This is ugly. Unfortunately, append returns the thing receiving the appendage, not the thing being appended.
+                    //Give it a tooltip
+                    if(rowHTML[rightsholderName].tooltipContent)
+                        cell.tooltip({ content: rowHTML[rightsholderName].tooltipContent });
+                    //Style it if it has a score
+                    if(rowHTML[rightsholderName].score !== "-")
+                        addScoreCategoryClass(cell, parseInt(rowHTML[rightsholderName].score));
+                    //When the user mouses over a cell, this makes the cell's column header become highlighted.
+                    //Row headers already do this via pure CSS. (You can put a hover selector on the <tr> to find the appropriate row head, but the column heads aren't exclusively enclosed
+                    //in an element with the cells below them, so we have to resort to javascript.) 
+                    cell.hover(function (event) {
+                        //On mouse hover, give the column header a class.
+                        $('#' + getColumnHeadID(rightsholderName)).addClass("hoveredColumn");
+                    },
+                    function (event) {
+                        //On mouse hover end, remove the class.
+                        $('#' + getColumnHeadID(rightsholderName)).removeClass("hoveredColumn");
+                    });
+                });
             });
 
             this.filter(monitorTables.backingData.length - 1);
@@ -176,20 +215,25 @@ function Matrix() {
 }
 var matrix = new Matrix();
 
+//TODO: These are ugly. Also, why are they in this file when ImpactedRights uses them too?
+//TODO: It would be neat to put stripNonAlphanumeric into the String prototype.
+function stripNonAlphanumeric(string) {
+    return string.replace(/\W/g, "");
+}
 
 function getColumnHeadID(columnName) {
-    var idString = "column" + columnName.replace(/\W/g, ""); //Makes non-alphanumeric into nothing.
+    var idString = "column" + stripNonAlphanumeric(columnName); //Makes non-alphanumeric into nothing.
     return idString;
 }
 
 function getRowID(rightName) {
-    var idString = "row" + rightName.replace(/\W/g, ""); //Makes non-alphanumeric into nothing.
+    var idString = "row" + stripNonAlphanumeric(rightName); //Makes non-alphanumeric into nothing.
     return idString;
 }
 
 function getDataCellClass(rightName, rightsHolderName) {
     var returnVal = {};
-    returnVal.rowClass = "row" + rightName.replace(/\W/g, "") + "Data";
-    returnVal.colClass = "column" + rightsHolderName.replace(/\W/g, "") + "Data";
+    returnVal.rowClass = "row" + stripNonAlphanumeric(rightName) + "Data";
+    returnVal.colClass = "column" + stripNonAlphanumeric(rightsHolderName) + "Data";
     return returnVal;
 }
