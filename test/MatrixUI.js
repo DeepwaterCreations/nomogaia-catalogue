76df9f231ad5109d21d0ab3f543a71335a805591
DeepwaterCreations <dylan.craine@deepwatercreations.com>
2015-02-27 11:55:43 -0700

    Matrix now filters out "context" catalog elements as per Kendyl's request.

--- test/MatrixUI.js
+++ test/MatrixUI.js
@@ -1,15 +1,4 @@
-﻿//So we have rows associated with rights, and columns associated with impacted individuals, and the cells should hold the values.
-//We also need to reflect that this data has changed over time. 
-//Tooltips: Reasons for the scores, maybe also trend over time. 
-//Also, we want to filter impacted rights holders so we don't see all of them.
-
-//index.html can hold a blank table, like for the rows table.
-//We want to: Iterate over the rights list, and for each item, add a table data with the name and then a series of table datas with the values for the list of impacted rights-holders.
-//We also want to listen to the appropriate data values.
-//And, we'll want tool tips on the table data elements. (So they'll need title='' !)
-
-
-function Matrix() {
+﻿function Matrix() {
 
     this.dirty = true;
 
@@ -31,7 +20,6 @@ function Matrix() {
 
     //We need to know which table we're rebuilding in the function.
     //We also don't want to do this unless something has legitimately changed.
-    //Maybe I want a monitor data structure lurking behind the UI that can keep track of such things?
     this.rebuild = function (monitor) {
         if (!this.dirty)
             return;
@@ -79,7 +67,13 @@ function Matrix() {
             
             //Generate the scores and push them into the htmlString.
             var rows = data.getRows("Impacted Rights", rightName);
+            rows = rows.filter(function (element, index, array) {
+                return element.getData("Catalog") !== "Context";
+            });
             var newestMonitorRows = newestMonitorData.getRows("Impacted Rights", rightName);
+            newestMonitorRows = newestMonitorRows.filter(function (element, index, array) {
+                return element.getData("Catalog") !== "Context";
+            });
             options.getColumnOptions("Impacted Rights-Holders").forEach(function (rightsholderName) {
                 rightsholderName = rightsholderName || undefinedRightsHolderNameFiller;
                 
