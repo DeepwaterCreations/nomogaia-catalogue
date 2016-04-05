739724a7e0071f778db2e23533ce4b2d8472122b
os-nicolas <Colin.Wielga@ncf.edu>
2016-04-05 16:59:10 -0400

    selected v2

--- test/TreeView.js
+++ test/TreeView.js
@@ -1,55 +1,69 @@
 function TreeView(categoryHierarchy) {
     this.hierarchy = categoryHierarchy;
     this.address = new Adress();
-    var that = this;
+
+
+    //var that = this;
     
-    this.update = function () {
-        var currentItems = this.hierarchy.itemsForAdress(this.address);
+    //this.update = function () {
+    //    var currentItems = this.hierarchy.itemsForAdress(this.address);
 
-        var uiList = $("#currentItems");
+    //    var uiList = $("#currentItems");
 
-        uiList.empty();
+    //    uiList.empty();
 
-        for (var i = 0; i < currentItems.length; i++) {
-            var str = "<li class='expandable'>" + currentItems[i] + "</li>";
-            uiList.append(str);
-        }
+    //    for (var i = 0; i < currentItems.length; i++) {
+    //        var str = "<li class='expandable'>" + currentItems[i] + "</li>";
+    //        uiList.append(str);
+    //    }
 
-        $(".expandable").click(function () {
-            var me = $(this);
-            var myName = me.text();
-            that.address.append(myName);
-            that.update();
-        });
+    //    $(".expandable").click(function () {
+    //        var me = $(this);
+    //        var myName = me.text();
+    //        that.address.append(myName);
+    //        that.update();
+    //    });
 
-        var adressBar = $("#adressBar");
-        adressBar.empty();
+    //    var adressBar = $("#adressBar");
+    //    adressBar.empty();
 
-        adressBar.append('<button id="home">home</button>');
-        $("#home").click(function () {
-            that.address.home();
-            that.update();
-        })
 
-        if (this.address.catalog !== null){
-            adressBar.append('<button id="catalog" class="adressButton">' + this.address.catalog + '</button>');
-        }
-        if (this.address.category !== null) {
-            adressBar.append('<button id="category" class="adressButton">' + this.address.category + '</button>');
-        }
-        if (this.address.subCategory !== null) {
-            adressBar.append('<button id="subCategory" class="adressButton">' + this.address.subCategory + '</button>');
-        }
-        if (this.address.topic !== null) {
-            adressBar.append('<button id="topic" class="adressButton">' + this.address.topic + '</button>');
-        }
-        $(".adressButton").click(function () {
-            that.address.rollBackTo($(this).text());
-            that.update();
-        });
-    }
+
+    //    var title = $("#title");
+
+    //    if (this.address.catalog !== null) {
+    //        adressBar.append('<button id="home">home</button>');
+    //        $("#home").click(function () {
+    //            that.address.home();
+    //            that.update();
+    //        })
+    //        if (this.address.category !== null) {
+    //            adressBar.append('<button id="catalog" class="adressButton">' + this.address.catalog + '</button>');
+    //            if (this.address.subCategory !== null) {
+    //                adressBar.append('<button id="category" class="adressButton">' + this.address.category + '</button>');
+    //                if (this.address.topic !== null) {
+    //                    //adressBar.append('<button id="subCategory" class="adressButton">' + this.address.subCategory + '</button>');
+    //                    //adressBar.append('<button id="topic" class="adressButton">' + this.address.topic + '</button>');
+    //                } else {
+    //                    title.text(this.address.subCategory);
+    //                }
+    //            } else {
+    //                title.text(this.address.category);
+    //            }
+    //        } else {
+    //            title.text(this.address.catalog);
+    //        }
+    //    } else {
+    //        title.text("home");
+    //    }
+
+    //    $(".adressButton").click(function () {
+    //        that.address.rollBackTo($(this).text());
+    //        that.update();
+    //    });
+    //}
     
-    this.update();
+    //this.update();
 
     //$("#up").click(function () {
     //    that.address.up();
@@ -59,32 +73,30 @@ function TreeView(categoryHierarchy) {
 
 
 
-    //var outerList = $("#outline");
-    //var outlineString = TreeView.treeFromDictionary(categoryHierarchy.hierarchy);
-    //outerList.append(outlineString);
-    //$(".show-hide").click(function () {
-    //    var me = $(this);
-    //    var current = me.attr('data-shown');
-    //    current = current == "true" ? "false" : "true";
-    //    me.attr('data-shown', current);
-    //    var text = current == "true" ? "hide" : "show";
-    //    me.text(text)
-    //});
+    var outerList = $("#outline");
+    var outlineString = TreeView.treeFromDictionary(categoryHierarchy.hierarchy);
+    outerList.append(outlineString);
+    $(".show-hide").click(function () {
+        var me = $(this);
+        var current = me.attr('data-shown');
+        current = current == "true" ? "false" : "true";
+        me.attr('data-shown', current);
+        var text = current == "true" ? "hide" : "show";
+        me.text(text)
+    });
 }
 
 // returns a html string 
-//TreeView.treeFromDictionary = function (dict) {
-//    var res = "";
-//    for (var key in dict) {
-//        res += "<li> " + key;
-//        res += '<button class="show-hide" data-shown="false">show</button>'
-//        // if dict[key] is not a topic recurse
-//        if (!(dict[key] instanceof Topic)) {
-//            res += " <ul>" + TreeView.treeFromDictionary(dict[key]) + " </ul>";
-//        }
-//        res += " </li>";
-//    }
-//    return res;
-//}
-
-// returns string
+TreeView.treeFromDictionary = function (dict) {
+    var res = "";
+    for (var key in dict) {
+        res += "<li> " + key;
+        res += '<button class="show-hide" data-shown="false">show</button>'
+        // if dict[key] is not a topic recurse
+        if (!(dict[key] instanceof Topic)) {
+            res += " <ul>" + TreeView.treeFromDictionary(dict[key]) + " </ul>";
+        }
+        res += " </li>";
+    }
+    return res;
+}
