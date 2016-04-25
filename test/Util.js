8ab4e73b50d425aa6b4b8b37f30f8cd44b795755
os-nicolas <Colin.Wielga@ncf.edu>
2016-04-25 15:17:10 -0400

    polish

--- test/Util.js
+++ test/Util.js
@@ -6,9 +6,19 @@ Util.checkVisible = function(elm, threshold) {
 
     var rect = elm.getBoundingClientRect();
     var viewHeight = Math.max(document.documentElement.clientHeight, window.innerHeight);
-    console.log(rect.bottom + " " + rect.top);
+    //console.log(rect.bottom + " " + rect.top);
     var above = rect.bottom + threshold < 0;
     var below = rect.top - threshold >= viewHeight;
 
     return !above && !below;
 }
+
+//Util.checkVisible = function (elm) {
+
+//    var vpH = $(window).height(), // Viewport Height
+//        st = $(window).scrollTop(), // Scroll Top
+//        y = $(elm).offset().top,
+//        elementHeight = $(elm).height();
+
+//    return ((y < (vpH + st)) && (y > (st - elementHeight)));
+//}
