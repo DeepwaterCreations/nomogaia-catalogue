dd63a055b12ad43c7bbacfd17ce0a835b0f9e5d6
os-nicolas <Colin.Wielga@ncf.edu>
2016-05-10 14:50:31 -0400

    auto format + removed commented code

--- test/TopicAdder.js
+++ test/TopicAdder.js
@@ -1,20 +1,20 @@
 
 var TopicAdder = function ($timeout) {
-   
-    this.addNewCatalog = function (treeView,catalog) {
+
+    this.addNewCatalog = function (treeView, catalog) {
         //this.$timeout(function () {
-            console.log("added new catalog", treeView);
-            treeView[catalog] = {};
+        console.log("added new catalog", treeView);
+        treeView[catalog] = {};
         //});
     }
     this.isNewCatalog = function (treeView, catalog) {
-        return DataOptions.isNotEmpty(catalog) && 
+        return DataOptions.isNotEmpty(catalog) &&
             treeView[catalog] == undefined;
     }
     this.addNewCategory = function (treeView, catalog, catagory) {
         //this.$timeout(function () {
-            console.log("added new category", treeView);
-            treeView[catalog][catagory] = {};
+        console.log("added new category", treeView);
+        treeView[catalog][catagory] = {};
         //});
     }
     this.isNewCategory = function (treeView, catalog, catagory) {
@@ -25,8 +25,8 @@ var TopicAdder = function ($timeout) {
     }
     this.addNewSubCategory = function (treeView, catalog, catagory, subCatagory) {
         //this.$timeout(function () {
-            console.log("added new sub category", treeView);
-            treeView[catalog][catagory][subCatagory] = [];
+        console.log("added new sub category", treeView);
+        treeView[catalog][catagory][subCatagory] = [];
         //});
     }
     this.isNewSubCategory = function (treeView, catalog, catagory, subCatagory) {
@@ -39,30 +39,28 @@ var TopicAdder = function ($timeout) {
     }
 
     this.addNewTopic = function (treeView, catalog, category, subCategory, topic, description, module, source) {
-        //??
+
         description = description || "";
         module = module || DataOptions.getDefaultValue("Module");
         source = source || "";
-        //this.$timeout(function () {
-            // add a row
-            console.log("added new topic", treeView);
-            var rowAt = null;
 
-            var myTopic = new Topic(catalog, category, subCategory, topic, description, module, source);
-            for (var tabIndex in g.getMonitorTables().backingData) {
-                if (tabIndex >= monitorTabs.getActiveMonitor()) {
-                    var myTable = g.getMonitorTables().backingData[tabIndex];
-                    if (rowAt == null) {
-                        rowAt = myTable.addRow(myTopic.toData(myTable));
-                        rowAt.data.setMonitor(monitorTabs.getActiveMonitorAsString());
-                    } else {
-                        var dataAt = rowAt.data;
-                        var newData = new RowData(myTable, dataAt);
-                        rowAt = myTable.addRow(newData);
-                    }
+        console.log("added new topic", treeView);
+        var rowAt = null;
+
+        var myTopic = new Topic(catalog, category, subCategory, topic, description, module, source);
+        for (var tabIndex in g.getMonitorTables().backingData) {
+            if (tabIndex >= monitorTabs.getActiveMonitor()) {
+                var myTable = g.getMonitorTables().backingData[tabIndex];
+                if (rowAt == null) {
+                    rowAt = myTable.addRow(myTopic.toData(myTable));
+                    rowAt.data.setMonitor(monitorTabs.getActiveMonitorAsString());
+                } else {
+                    var dataAt = rowAt.data;
+                    var newData = new RowData(myTable, dataAt);
+                    rowAt = myTable.addRow(newData);
                 }
             }
-        //});
+        }
     }
 
     this.isNewTopic = function (treeView, catalog, catagory, subCatagory, topic) {
