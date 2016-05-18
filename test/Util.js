16a04bfcb4690cde5be625f307ddfa205039ec10
os-nicolas <Colin.Wielga@ncf.edu>
2016-05-18 15:45:21 -0400

    we now union the right a files had last time it was used and the default rights

--- test/Util.js
+++ test/Util.js
@@ -20,6 +20,21 @@ Util.disToCenter = function (elm) {
     return Math.abs( ((rect.bottom +rect.top)/2) - (viewHeight/2));
 }
 
+// takes two list
+// the list are assume to have no duplcates
+Util.union = function (lst1, lst2) {
+    var res = [];
+    for (var i = 0; i < lst1.length; i++) {
+        res.push(lst1[i]);
+    }
+    for (var i = 0; i < lst2.length; i++) {
+        if (res.indexOf(lst2[i])!=-1) {
+            res.push(lst2[i])
+        }
+    }
+    return res;
+}
+
 //Util.checkVisible = function (elm) {
 
 //    var vpH = $(window).height(), // Viewport Height
