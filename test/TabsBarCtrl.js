42a34646aba298f163bd1e6667fb8d475d75659e
DeepwaterCreations <dylan.craine@deepwatercreations.com>
2016-06-07 13:17:56 -0600

    Add "Save As" button

--- test/TabsBarCtrl.js
+++ test/TabsBarCtrl.js
@@ -1,5 +1,9 @@
 g.aspenApp.controller('tabBarController', ['$scope', function($scope){
 
+    $scope.clickSave = function(){
+        SaveLoad.checkSave();
+    };
+
     $scope.clickSaveAs = function(){
         SaveLoad.saveAs();
     };
